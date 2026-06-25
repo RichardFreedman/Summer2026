360dc5b06a25918d4f5632bcf904b10acfc27555
MHF3 <86084247+MHF3@users.noreply.github.com>
2026-06-24 21:29:23 -0400

    Update Lyric_Search.py

--- PARADISEC/Streamlit/pages/Lyric_Search.py
+++ PARADISEC/Streamlit/pages/Lyric_Search.py
@@ -1,1 +1,7 @@
-# TODO
+import streamlit as st
+
+if ('email' not in st.session_state or 'password' not in st.session_state): st.header('Login First!')
+elif (st.session_state.email == '' or st.session_state.password == ''): st.header('Login First!')
+else:
+    # TODO
+    st.header('In Progress')
