194545a373d77ccb8ba3eefebd3f4768d5614dd5
yekratsh <143466752+yekratsh@users.noreply.github.com>
2026-07-21 13:37:45 -0700

    Fixed bug to limit memory usage on large files

--- Multipage_PDF_OCR/multipage_extraction.py
+++ Multipage_PDF_OCR/multipage_extraction.py
@@ -1,4 +1,3 @@
-
 import os
 os.environ["OBJC_DISABLE_INITIALIZE_FORK_SAFETY"] = "YES"
 from paddleocr import PaddleOCR
@@ -6,6 +5,7 @@ import multiprocessing as mp
 import pymupdf
 import os
 import shutil
+import gc
 
 def extract_page(path):
     num=int(path[::-1][4:path[::-1].index("e")][::-1])
@@ -14,7 +14,10 @@ def extract_page(path):
     texts = []
     for item in result:
         texts.extend(item.get('rec_texts', []))
+    del result
+    gc.collect()
     return {"pagenum":num, "text":texts}
+
 
 if __name__ == "__main__":
     pdfpath=input("Path to PDF: ")
@@ -27,7 +30,7 @@ if __name__ == "__main__":
         dump_dir=res
     os.makedirs(dump_dir, exist_ok=True)
     for i, page in enumerate(document):
-        img = page.get_pixmap(dpi=75) #Can increase/lower DPI value for better accuracy/speed respectively
+        img = page.get_pixmap(dpi=85) #Can increase/lower DPI value for better accuracy/speed respectively
         img.save(f"{dump_dir}/page{i}.png")
     document.close()
 
